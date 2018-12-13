90814173679cfc8e4d1ffa6e71f55f1c4919f541
Ricardo Maldonado <maldonadoricardo93@gmail.com>
2018-12-12 23:37:22 -0300

    Arreglado select tipo documento

---
+++ TPFinal-Maldonado/src/app/model/enums/enum-tipo-documento.enum.ts
@@ -0,0 +1,7 @@
+export enum EnumTipoDocumento {
+    LE,
+    LC ,
+    DNI,
+    Pasaporte ,
+    CI,
+}
--- TPFinal-Maldonado/src/app/shared/alumno/alumno.component.ts
+++ TPFinal-Maldonado/src/app/shared/alumno/alumno.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, Output} from '@angular/core';
 import { IAlumno } from '../../model/interfaces/ialumno';
 import { ActivatedRoute, Params } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EnumTipoDocumento } from 'src/app/model/enums/enum-tipo-documento.enum';
 
 @Component({
   selector: 'app-alumno',
@@ -13,19 +14,23 @@ export class AlumnoComponent implements OnInit {
   //@Input() unAlumno:IAlumno;
   //alumno : { nombre:string};
   formulario:FormGroup;
+  EDocumento : string[];
 
   constructor( private _route : ActivatedRoute, private fb:FormBuilder ) { 
        
-    this.formulario = fb.group({
-      nombre:[null, Validators.required],
-      apellido:[null, Validators.required],
-      fecha_nacimiento :[null, Validators.required],
-      tipo_doc:[null, Validators.required],
-      nro_doc: [null, Validators.compose([Validators.required, Validators.maxLength(8)])],
-      comunidad:[null],
-      leg_utn:[null, Validators.required],
-      curso:[null, Validators.required],
-    })
+      this.EDocumento = Object.keys(EnumTipoDocumento);
+      this.EDocumento = this.EDocumento.slice(this.EDocumento.length / 2);
+
+      this.formulario = fb.group({
+        nombre:[null, Validators.required],
+        apellido:[null, Validators.required],
+        fecha_nacimiento :[null, Validators.required],
+        tipo_doc:[null, Validators.required],
+        nro_doc: [null, Validators.compose([Validators.required, Validators.maxLength(8)])],
+        comunidad:[null],
+        leg_utn:['XXXXXX-XX', Validators.required],
+        curso:[null, Validators.required],
+      })
   }
 
   ngOnInit() {
@@ -33,6 +38,10 @@ export class AlumnoComponent implements OnInit {
     this._route.params.subscribe( (params : Params) =>{ this.alumno.nombre = params.nombre;}  )
     console.log( this.alumno.nombre);
     console.log ( this.alumno , this.unAlumno);*/
+  }
+
+  onSubmit($value){
+    console.log($value);
   }
 
 }
